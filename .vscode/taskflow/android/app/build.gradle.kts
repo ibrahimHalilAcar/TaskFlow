2334b13f626b9d02cf401f207f065b0eed58f661
TheL0nely0bserver <ibrahimhalilacar93@gmail.com>
2026-05-31 00:40:20 +0300

    son pls

--- .vscode/taskflow/android/app/build.gradle.kts
+++ .vscode/taskflow/android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
-        isCoreLibraryDesugaringEnabled = false
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
@@ -39,4 +39,5 @@ flutter {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
